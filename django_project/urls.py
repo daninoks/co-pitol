3ecf409dd573a6efca84c9d32511c9b385e5dac9
daninoks <daninoks@protonmail.com>
2022-08-30 02:19:01 +0400

    urls for webhook

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from pilotCore.views import TelegramBotWebhookView
+
 
 urlpatterns = [
+    path('webhook/', csrf_exempt(TelegramBotWebhookView.as_view())),
     path('admin/', admin.site.urls),
 ]
